89ce47ab91bcedba85e8803aa069c4ab471bc977
Matthias Mueller <matthiasm@live.de>
2019-01-02 13:08:30 +0100

    WIP

--- src/areas/services/servants/implementation/index.ts
+++ src/areas/services/servants/implementation/index.ts
@@ -1,4 +1,4 @@
-export * from './barrel-file-repository.service';
 export * from './typescript-files-searching.servant';
 export * from './glob-factory.servant';
 export * from './folder.servant';
+export * from './barrel-file-repository.service';
