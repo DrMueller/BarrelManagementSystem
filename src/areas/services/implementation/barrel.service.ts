508aeb71ccecb90f45c719918d30105128f00260
Matthias Müller <MatthiasM@live.de>
2019-02-03 22:03:31 +0100

    WIP

--- src/areas/services/implementation/barrel.service.ts
+++ src/areas/services/implementation/barrel.service.ts
@@ -4,7 +4,7 @@ import { ExportEntry } from '../../models';
 import { IBarrelService } from '../barrel-service.interface';
 import {
   BarrelFileRepositoryServiceName, IBarrelFileRepositoryService,
- ITypeScriptFilesSearchingServant, TypeScriptFilesSearchingServantName
+  ITypeScriptFilesSearchingServant, TypeScriptFilesSearchingServantName
 } from '../servants';
 
 @injectable()
@@ -16,13 +16,19 @@ export class BarrelService implements IBarrelService {
 
   public async alignBarrelInDirecoryAsync(directoryPath: string): Promise<void> {
     const barrelFile = await this.barrelFileReposistory.loadOrCreateAsync(directoryPath);
-    const filesInDirectory = await this.fileServant.searchTypeScriptFilesAsync(directoryPath);
+    const filesInDirectory = await this.loadFilesToConsiderFromDirectoryAsync(directoryPath);
     const exportEntries = filesInDirectory.map(file => ExportEntry.createFromFilePath(file));
-
     const alignmentResult = barrelFile.alignExportEntries(exportEntries);
 
     if (alignmentResult.itemsChanged) {
       await this.barrelFileReposistory.saveAsync(barrelFile);
     }
+  }
+
+  private async loadFilesToConsiderFromDirectoryAsync(directoryPath: string): Promise<string[]> {
+    let filesInDirectory = await this.fileServant.searchTypeScriptFilesAsync(directoryPath);
+    filesInDirectory = filesInDirectory.filter(fileName => !fileName.toLowerCase().endsWith('spec.ts'));
+
+    return filesInDirectory;
   }
 }
